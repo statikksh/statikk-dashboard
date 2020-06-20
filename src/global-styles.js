626cf49c54b710c5340293395dca3a816ec17cfc
7wf <7ywolf@gmail.com>
2020-06-20 19:23:04 -0300

    Update global styles

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,17 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
-
     * {
         padding: 0;
         margin: 0;
+        box-sizing: border-box;
     }
 
     body {
-        font-family: 'Roboto', monospace;
+        font-family: 'Roboto', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
-
 `;
