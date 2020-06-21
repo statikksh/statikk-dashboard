cfe6e580e209b48232a2b56e0ef8b70aaeb6b934
7wf <7ywolf@gmail.com>
2020-06-21 18:58:49 -0300

    Create component file for containers

---
+++ src/components/shared/containers.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+/**
+ * The container for sign-in/sign-up pages.
+ */
+const SmallContainer = styled.div`
+    width: 235px;
+    margin: 6rem auto;
+`;
+
+export { SmallContainer };
